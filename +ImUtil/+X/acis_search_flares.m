885e7fa44c9086225b1f1f8aa352500b061cc706
Eran Ofek <ofek.eran@gmail.com>
2018-12-20 17:28:13 +0200

    fix bugs in @AstCat/match_with_cat.m + add ds9.plotc

--- +ImUtil/+X/acis_search_flares.m
+++ +ImUtil/+X/acis_search_flares.m
@@ -16,6 +16,7 @@ function []=acis_search_flares(varargin)
 CatField = AstCat.CatField;
 ColField = AstCat.ColField;
 
+DefV.EvtFileName          = 'acis.evt2';
 DefV.EnergyRange          = [200 10000];
 DefV.SelectGoodTimes      = true;
 DefV.NinBin               = 100;
@@ -29,22 +30,48 @@ DefV.timefield            = 'time';
 DefV.ccdfield             = 'ccd_id';
 DefV.chipxfield           = 'chipx';
 DefV.chipyfield           = 'chipy';
+DefV.xfield               = 'x';
+DefV.yfield               = 'y';
+
 InPar = InArg.populate_keyval(DefV,varargin,mfilename);
 
 
-[EvtT,MetaData,Image] = ImUtil.X.read_chandra_acis('acis.evt2',...
+[EvtT,MetaData,Image] = ImUtil.X.read_chandra_acis(InPar.EvtFileName,...
                             'EnergyRange',InPar.EnergyRange,...  
                             'SelectGoodTimes',InPar.SelectGoodTimes,...
                             'NinBin',InPar.NinBin');
 
 EvtT = astcat_array2table(EvtT);                        
-UniqueCCD = unique(EvtT.(CatField).(InPar.ccdfield));
-Nccd      = numel(UniqueCCD);
+
+%UniqueCCD = unique(EvtT.(CatField).(InPar.ccdfield));
+%Nccd      = numel(UniqueCCD);
+
+
+% This should work in sky coordinates (x,y)
+MinX = min(EvtT.Cat.(InPar.xfield));
+MinY = min(EvtT.Cat.(InPar.yfield));
+MaxX = max(EvtT.Cat.(InPar.xfield));
+MaxY = max(EvtT.Cat.(InPar.yfield));
+
+% construct a full image
+% on scale of ~100x100 pix:
+% 1. get local exp map
+% 2. estimate back
+% 3. run MF
+% 4. Repeat on 1000 s image
+% save LC for all cand in ~90% circle.
+
+
+
+
+
 for Iccd=1:1:Nccd
     FlagCCD = EvtT.(CatField).(InPar.ccdfield) == UniqueCCD(Iccd);
     
     % generate image in order to remove bright sources and estimate
     % background level
+    % Note that sky coordinates (x,y) should be used
+    
     VecX = (1:InPar.BrightPixSize:InPar.ChipSize(1))';
     VecY = (1:InPar.BrightPixSize:InPar.ChipSize(2))';
 
@@ -52,6 +79,9 @@ for Iccd=1:1:Nccd
                          EvtT.(CatField).(InPar.chipyfield)(FlagCCD),...
                          VecX,VecY);
     
+                     
+    % do this for FlareExp time blocks...
+                     
     CntRate  = sum(ImageB(:))./MetaData.ExpTime./prod(InPar.ChipSize);
     Fback    = ImageB<max(2,median(ImageB(:))+std(ImageB(:)).*5);
     BackRate = sum(ImageB(Fback))./MetaData.ExpTime./prod(InPar.ChipSize);
