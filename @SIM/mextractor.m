2a8d94e862706c2738797622b19f9ab24115f89c
Eran Ofek <ofek.eran@gmail.com>
2019-01-06 14:54:21 +0200

    New FluxClass

--- @SIM/mextractor.m
+++ @SIM/mextractor.m
@@ -958,7 +958,9 @@ for Isim=1:1:Nsim
     
     % WCS positions
     % % fixing a bug in WCS - need to fix this within SIM...
+    Isim
     Wsim = ClassWCS.populate(Sim(Isim));
+   
     if (Calc.CooPeak)
         [AlphaPeak,DeltaPeak] = xy2coo(WSim,[PeakX,PeakY]);   % radians
         
