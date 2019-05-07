ce312f60070f4e0e7b8130e0bedb2456db2552ce
nganot <noam.ganot@weizmann.ac.il>
2019-05-07 16:47:52 +0300

    Extinction calculation
    Use synphot to calculate the extinction when it used to calculate the magintude instead of previous implementation which used an effective wave length extinction to change the magnitude.

--- +AstroUtil/+supernova/sn_cooling_sw.m
+++ +AstroUtil/+supernova/sn_cooling_sw.m
@@ -272,14 +272,18 @@ if (nargout>3) && (~isempty(InPar.FiltFam) || ~isempty(InPar.Wave))
         Spec=Spec.*SpecFactor(:);
         clear F
 
-        Mag = synphot(Spec,Filter,[],InPar.FiltSys);
+        if (InPar.Ebv>0)
+            Mag = synphot(Spec,Filter,[],InPar.FiltSys,'cos',InPar.Ebv,InPar.Rv);
+        else
+            Mag = synphot(Spec,Filter,[],InPar.FiltSys);
+        end
     else 
         [~,In]=AstroUtil.spec.black_body(Tc./(1+InPar.redshift),InPar.Wave).*(1+InPar.redshift).^4;
         Mag = convert.flux(In.*R.^2./(Dist.*Pc).^2,'cgs/Hz','AB',InPar.Wave,'A');
-    end
-    if (InPar.Ebv>0)
-        A = AstroUtil.spec.extinction(InPar.Ebv,InPar.FiltFam,InPar.FiltName,InPar.Rv);
-        Mag = Mag + A;
+        if (InPar.Ebv>0)
+            A = AstroUtil.spec.extinction(InPar.Ebv,InPar.FiltFam,InPar.FiltName,InPar.Rv);
+            Mag = Mag + A;
+        end
     end
     Mag = reshape(Mag,S);
 else
