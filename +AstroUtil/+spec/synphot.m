05a70b6b97c70c2a7eaa5c4d7b3b69539fc940fe
nganot <noam.ganot@weizmann.ac.il>
2019-06-11 14:54:03 +0300

    Update synphot.m
    Change if strcmpi to switch
    Add bux fix for cell array with only one spectrum to calculate.

--- +AstroUtil/+spec/synphot.m
+++ +AstroUtil/+spec/synphot.m
@@ -96,7 +96,14 @@ if (iscell(Spec))
         error('MAAT:AstroUtils:spec:synphot:spec_wave_no_vector',...
             'Error. The first component of the Spec cell must be a vector.');
     else
-        if ismatrix(Spec{2})
+        if isvector(Spec{2})
+            if length(Spec{2})~=length(Spec{1})
+                error('MAAT:AstroUtils:spec:synphot:spec_wrong_elements',...
+                    'Error. The number of elements in the two components of the Spec cell must be in agreement with eachother.');
+            else
+                Spec{2}=Spec{2}(:);
+            end
+        elseif ismatrix(Spec{2})
             if size(Spec{2},1)~=length(Spec{1})
                 if size(Spec{2},2)==length(Spec{1})
                     Spec{2} = Spec{2}.';
@@ -104,11 +111,6 @@ if (iscell(Spec))
                     error('MAAT:AstroUtils:spec:synphot:spec_wrong_elements',...
                         'Error. One of the two dimensions of the second component of the Spec cell must be in agreement with the number of elements of the first component.');
                 end
-            end
-        elseif isvector(Spec{2})
-            if length(Spec{2})~=length(Spec{1})
-                error('MAAT:AstroUtils:spec:synphot:spec_wrong_elements',...
-                    'Error. The number of elements in the two components of the Spec cell must be in agreement with eachother.');
             end
         else
             error('MAAT:AstroUtils:spec:synphot:spec_wrong_elements',...
@@ -141,6 +143,9 @@ switch lower(Algo)
         % Interp transminssion curve on Spec
         if iscell(Spec)
             [Spec,Tran]     = AstroUtil.spec.eq_sampling(Spec,Tran,Spec{1},InterpMethod);
+            if isvector(Spec{2})
+                Spec{2}=Spec{2}(:);
+            end
 %             I = find(~isnan(Tran(:,2)));
 %             Spec{1} = Spec{1}(I,:);
 %             Spec{2} = Spec{2}(I,:);
@@ -193,25 +198,30 @@ switch lower(Algo)
  %           NormTran = 1;
             Fnu      = NaN;
         else
-            if strcmpi(Device,'bol')
-                NormTran = trapz(Freq,Tran(:,2));
-                Fnu      = trapz(Freq,SpecFnu.*Tran(:,2))./NormTran;
-                %Flam     = convert_flux(Fnu,'cgs/Hz','cgs/A',FiltEffWave,'A');
-            elseif strcmpi(Device,'photon')
-                NormTran = trapz(Freq,Tran(:,2)./Freq);
-                Fnu      = trapz(Freq,SpecFnu.*Tran(:,2)./Freq)./NormTran;
+            switch lower(Device)
+                case 'bol'
+                    NormTran = trapz(Freq,Tran(:,2));
+                    Fnu      = trapz(Freq,SpecFnu.*Tran(:,2))./NormTran;
+                case'photon'
+                    NormTran = trapz(Freq,Tran(:,2)./Freq);
+                    Fnu      = trapz(Freq,SpecFnu.*Tran(:,2)./Freq)./NormTran;
+                otherwise
+                    error('Unknown Device option');
             end
         end
         Mag      = -48.6 - 2.5.*log10(Fnu);
         
      case 'vega'
         load vega_spec.mat;
-        if strcmpi(Device,'bol')
-            VegaF   = AstroUtil.spec.eq_sampling(vega_spec,Tran,Tran(:,1));
-    %        Freq    = convert.energy('A','Hz',Tran(:,1));
-            Fvega   = trapz(Tran(:,1),Spec(:,2).*Tran(:,2)./VegaF(:,2));
-        elseif strcmpi(Device,'photon')
-            error('Number of photon formula for vega was not implemented');
+        switch lower(Device)
+            case 'bol'
+                VegaF   = AstroUtil.spec.eq_sampling(vega_spec,Tran,Tran(:,1));
+        %        Freq    = convert.energy('A','Hz',Tran(:,1));
+                Fvega   = trapz(Tran(:,1),Spec(:,2).*Tran(:,2)./VegaF(:,2));
+            case 'photon'
+                error('Number of photon formula for vega was not implemented');
+            otherwise
+                error('Unknown Device option');           
         end
         Mag     = -2.5.*log10(Fvega);
 
