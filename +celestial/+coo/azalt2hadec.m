a438a2bdfb4fd4406ee58fe178459a62ddb9b81d
Eran Ofek <ofek.eran@gmail.com>
2020-08-10 19:33:53 +0300

    celestial.coo Az/Alt->HA/Dec and vice versa

---
+++ +celestial/+coo/azalt2hadec.m
@@ -0,0 +1,33 @@
+function [HA,Dec]=azalt2hadec(Az,Alt,Lat,CooUnits)
+% Convert Az/Alt to HA/Dec
+% Package: +celestial.coo
+% Input  : - Az [rad]
+%          - Alt [rad]
+%          - Lat [rad]
+%          - CooUnits {'rad' | 'deg'}. Default is 'rad'.
+% Output : - HA [same as input]
+%          - Dec [same as input]
+%      By : Eran O. Ofek                Aug 2020
+% Example: [HA,Dec]=celestial.coo.azalt2hadec(Az,Alt,Lat)
+
+if nargin==4
+    Convert = convert.angular(CooUnits,'rad');
+    Az  = Az.*Convert;
+    Alt = Alt.*Convert;
+    Lat = Lat.*Convert;
+end
+
+SinDec = sin(Alt).*sin(Lat) + cos(Alt).*cos(Az).*cos(Lat);
+CosDec = sqrt(1 - SinDec.*SinDec);
+
+SinHA  = (-cos(Alt).*sin(Az))./CosDec;
+CosHA  = (sin(Alt).*cos(Lat) - cos(Alt).*cos(Az).*sin(Lat))./CosDec;
+HA     = atan2(SinHA, CosHA);
+Dec    = asin(SinDec);
+
+
+if nargin==4
+    Convert = convert.angular('rad',CooUnits);
+    HA  = HA.*Convert;
+    Dec = Dec.*Convert;
+end
