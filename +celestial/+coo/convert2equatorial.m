dc82e661decfb567dd1bbbdd88af6aa83c3be943
Enrico Segre <enrico.segre@weizmann.ac.il>
2021-02-14 09:12:01 +0000

    add output to convert2equatorial.m

--- +celestial/+coo/convert2equatorial.m
+++ +celestial/+coo/convert2equatorial.m
@@ -151,18 +151,19 @@ AppAlt = TrueAlt + Refraction;
 % calculate LST
 LST = celestial.time.lst(InPar.JD,InPar.ObsCoo(1)./RAD,'a');  % fraction of day
 % calculate HA = LST - RA
-AppHA = 2.*pi.*LST - AppRA;
+AppHA = 2.*pi.*LST - AppRA;   % [rad]
 % call distortions function
 if isempty(InPar.DistFun)
     DeltaDistHA  = 0; % deg
     DeltaDistDec = 0; % deg
-else
-    [DeltaDistHA, DeltaDistDec] = InPar.DistFun(AppHA,AppDec);
-end
+    
+    DistHA  = AppHA  + DeltaDistHA./RAD;
+    DistRA  = AppRA  + DeltaDistHA./RAD;
+    DistDec = AppDec + DeltaDistDec./RAD;
 
-DistHA  = AppHA  + DeltaDistHA./RAD;
-DistRA  = AppRA  + DeltaDistHA./RAD;
-DistDec = AppDec + DeltaDistDec./RAD;
+else
+    [DistHA, DistDec] = InPar.DistFun(AppHA,AppDec);
+end
 
 Aux.JD        = InPar.JD;
 Aux.LST       = LST;  % fraction of day
